8cd8b29be0d1cc6303fda790f19f650ed6bd8c04
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2024-05-16 15:35:02 +0530

    Create _8160_q2.java

---
+++ Assignment1/_8160_q2.java
@@ -0,0 +1,50 @@
+class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+public class _8160_q2 {
+	public static void main(String[] args) {
+		Rectangle myRectangle = new Rectangle(10.0, 14.0);
+
+        System.out.println("Initial values - Length: " + myRectangle.getLength() +
+                ", Width: " + myRectangle.getWidth());
+
+        myRectangle.setLength(10.0);
+        myRectangle.setWidth(7.0);
+
+        System.out.println("Updated values - Length: " + myRectangle.getLength() +
+                ", Width: " + myRectangle.getWidth());
+
+        System.out.println("The Area: " + myRectangle.calculateArea());
+        System.out.println("The Perimeter: " + myRectangle.calculatePerimeter());
+	}
+}
