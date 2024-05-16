82fc55e77fe8a543e1fdecf220686c842263ead7
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2024-05-16 16:45:43 +0530

    Create q1.java

---
+++ Assignment4/q1.java
@@ -0,0 +1,13 @@
+
+public class q1 {
+
+	public static void main(String[] args) {
+		try {
+			String str = null;
+			System.out.println("The length of the string is : " + str.length());
+		} catch (NullPointerException e) {
+			System.out.println("NullPointerException caught: " + e.getMessage());
+		}
+	}
+
+}
