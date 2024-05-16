bbaffdab699e4b79a25c4a38f6de16fc18c7bf0b
Pranab Rout <118814281+ipranab@users.noreply.github.com>
2024-05-16 17:16:55 +0530

    Create q1.java

---
+++ Assignment5/q1.java
@@ -0,0 +1,28 @@
+public class Q1 {
+    private String name;
+
+    public Q1(String name) {
+        this.name = name;
+    }
+
+    public void show() {
+        Q1 b = new Q1("Inner Object");
+        b.display();
+    }
+
+    public void display() {
+        Q1 a = new Q1("Inner display object");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Garbage collected: " + name);
+        super.finalize();
+    }
+
+    public static void main(String[] args) {
+        Q1 obj = new Q1("Main object");
+        obj.show();
+        System.gc();
+    }
+}
